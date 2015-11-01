4fd432c49b5e760520505cc96470baa7f4edb5b9
Володя Володин <vovanblya@mail.ru>
2015-11-01 13:02:58 +0400

    - add lexicon

--- core/components/ecc/lexicon/en/default.inc.php
+++ core/components/ecc/lexicon/en/default.inc.php
@@ -2,32 +2,4 @@
 include_once 'setting.inc.php';
 
 $_lang['ecc'] = 'ecc';
-$_lang['ecc_menu_desc'] = 'A sample Extra to develop from.';
-$_lang['ecc_intro_msg'] = 'You can select multiple items by holding Shift or Ctrl button.';
-
-$_lang['ecc_items'] = 'Items';
-$_lang['ecc_item_id'] = 'Id';
-$_lang['ecc_item_name'] = 'Name';
-$_lang['ecc_item_description'] = 'Description';
-$_lang['ecc_item_active'] = 'Active';
-
-$_lang['ecc_item_create'] = 'Create Item';
-$_lang['ecc_item_update'] = 'Update Item';
-$_lang['ecc_item_enable'] = 'Enable Item';
-$_lang['ecc_items_enable'] = 'Enable Items';
-$_lang['ecc_item_disable'] = 'Disable Item';
-$_lang['ecc_items_disable'] = 'Disable Items';
-$_lang['ecc_item_remove'] = 'Remove Item';
-$_lang['ecc_items_remove'] = 'Remove Items';
-$_lang['ecc_item_remove_confirm'] = 'Are you sure you want to remove this Item?';
-$_lang['ecc_items_remove_confirm'] = 'Are you sure you want to remove this Items?';
-
-$_lang['ecc_item_err_name'] = 'You must specify the name of Item.';
-$_lang['ecc_item_err_ae'] = 'An Item already exists with that name.';
-$_lang['ecc_item_err_nf'] = 'Item not found.';
-$_lang['ecc_item_err_ns'] = 'Item not specified.';
-$_lang['ecc_item_err_remove'] = 'An error occurred while trying to remove the Item.';
-$_lang['ecc_item_err_save'] = 'An error occurred while trying to save the Item.';
-
-$_lang['ecc_grid_search'] = 'Search';
-$_lang['ecc_grid_actions'] = 'Actions';
+$_lang['ecc_welcome'] = 'Welcome...';
