5976b7516099b28373795faeeafa9681b17038a8
Володя Володин <vovanblya@mail.ru>
2015-11-01 16:20:48 +0400

    - add lexicon

--- core/components/ecc/lexicon/en/properties.inc.php
+++ core/components/ecc/lexicon/en/properties.inc.php
@@ -1,8 +1,11 @@
 <?php
 
-$_lang['ecc_prop_limit'] = 'The number of Items to limit per page.';
-$_lang['ecc_prop_outputSeparator'] = 'A string to separate each row with.';
-$_lang['ecc_prop_sortBy'] = 'The field to sort by.';
-$_lang['ecc_prop_sortDir'] = 'The direction to sort by.';
-$_lang['ecc_prop_tpl'] = 'The chunk to use for each row of Items.';
-$_lang['ecc_prop_toPlaceholder'] = 'If set, will output the content to the placeholder specified in this property, rather than outputting the content directly.';
+$_lang['ecc_prop_namespace'] = 'Пространство имён';
+$_lang['ecc_prop_controller'] = 'Контроллер';
+$_lang['ecc_prop_path'] = 'Путь к файлу контроллера';
+$_lang['ecc_prop_tplOuter'] = 'Чанк обертка';
+$_lang['ecc_prop_frontendCss'] = 'Файл с css стилями для подключения на фронтенде';
+$_lang['ecc_prop_frontendJs'] = 'Файл с javascript для подключения на фронтенде';
+$_lang['ecc_prop_mainCss'] = 'Файл с css стилями Extjs для подключения на фронтенде';
+$_lang['ecc_prop_mainJs'] = 'Файл с javascript Extjs для подключения на фронтенде';
+$_lang['ecc_prop_location'] = 'Место нахождения файлов';
--- core/components/ecc/lexicon/ru/properties.inc.php
+++ core/components/ecc/lexicon/ru/properties.inc.php
@@ -1,8 +1,11 @@
 <?php
 
-$_lang['ecc_prop_limit'] = 'Ограничение вывода Предметов на странице.';
-$_lang['ecc_prop_outputSeparator'] = 'Разделитель вывода строк.';
-$_lang['ecc_prop_sortBy'] = 'Поле сортировки.';
-$_lang['ecc_prop_sortDir'] = 'Направление сортировки.';
-$_lang['ecc_prop_tpl'] = 'Чанк оформления каждого ряда Предметов.';
-$_lang['ecc_prop_toPlaceholder'] = 'Усли указан этот параметр, то результат будет сохранен в плейсхолдер, вместо прямого вывода на странице.';
+$_lang['ecc_prop_namespace'] = 'Пространство имён';
+$_lang['ecc_prop_controller'] = 'Контроллер';
+$_lang['ecc_prop_path'] = 'Путь к файлу контроллера';
+$_lang['ecc_prop_tplOuter'] = 'Чанк обертка';
+$_lang['ecc_prop_frontendCss'] = 'Файл с css стилями для подключения на фронтенде';
+$_lang['ecc_prop_frontendJs'] = 'Файл с javascript для подключения на фронтенде';
+$_lang['ecc_prop_mainCss'] = 'Файл с css стилями Extjs для подключения на фронтенде';
+$_lang['ecc_prop_mainJs'] = 'Файл с javascript Extjs для подключения на фронтенде';
+$_lang['ecc_prop_location'] = 'Место нахождения файлов';
